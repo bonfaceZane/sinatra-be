a65a4cb657be52a3f44524c0e4c3f6a36c5a5837
zaneObwoni <zaneobwoni@gmail.com>
2023-03-15 14:33:26 +0100

    added request file

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,13 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
+async fn create_user(// this argument tells axum to parse the request body
 ) -> (StatusCode, Json<User>) {
-    // insert your application logic here
     let user = User {
         id: 13373,
-        username: payload.username,
+        username: "zane".to_owned(),
     };
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
     (StatusCode::CREATED, Json(user))
 }
 
@@ -56,12 +50,12 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", get(create_user));
     // .route("/users/:id", get(get_user));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
