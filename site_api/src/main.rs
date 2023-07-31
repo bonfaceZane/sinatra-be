29a62154695e51d831278cbf32e19d4308f249db
zaneObwoni <zaneobwoni@gmail.com>
2023-08-01 01:19:20 +0200

    added editor config

--- site_api/src/main.rs
+++ site_api/src/main.rs
@@ -3,5 +3,11 @@ use shared::Post;
 fn main() {
     let new_post = Post::new("Global warming".to_owned(), "New body".to_owned());
 
-    dbg!(new_post)
+    let second_post = Post::new(
+        "Best losless music".to_owned(),
+        "Create.Share.Make".to_owned(),
+    );
+
+    dbg!(new_post);
+    dbg!(second_post);
 }
