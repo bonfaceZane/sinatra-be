77f23e256c9a2ee457f613c16650b62dc5a8e6cf
zaneObwoni <zaneobwoni@gmail.com>
2023-08-01 02:19:57 +0200

    all modules working fine

--- site_api/src/main.rs
+++ site_api/src/main.rs
@@ -1,13 +1,17 @@
+use image_api::Image;
 use shared::Post;
 
 fn main() {
-    let new_post = Post::new("Global warming".to_owned(), "New body".to_owned());
+    let new_post = Post::new("Global warming".to_string(), "New body".to_string());
 
     let second_post = Post::new(
         "Best losless music".to_owned(),
         "Create.Share.Make".to_owned(),
     );
 
+    let get_new_image = Image::new("perfomant image".to_owned());
+
     dbg!(new_post);
     dbg!(second_post);
+    dbg!(get_new_image);
 }
