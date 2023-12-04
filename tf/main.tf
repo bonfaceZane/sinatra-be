87be49ad6103e646cc1d46dc7a527c2948c8eadc
zaneObwoni <zaneobwoni@gmail.com>
2023-12-05 00:54:32 +0100

    updated vars

--- tf/main.tf
+++ tf/main.tf
@@ -13,7 +13,7 @@ provider "mongodbatlas" {
 
 resource "mongodbatlas_cluster" "poy_terraform" {
     name                        = "test-sinatra-db"
-    project_id                  = var.atlasprojectid
+    project_id                  = "7d424a9c-2cb9-4b34-b307-c397e1461806"
     provider_instance_size_name = ""
     provider_name               = ""
 }
